042aa86c42fa668f540506b4d9e51999a23005f1
naoki <n.globe.us@gmail.com>
2024-01-19 07:56:51 +0900

    app/ を 予め build して dis/にいれておく

--- src/app/vite.config.ts
+++ src/app/vite.config.ts
@@ -3,12 +3,11 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  root: path.resolve(process.cwd(), 'src', 'app'),
   plugins: [
     react(),
   ],
   build: {
     // 出力ディレクトリの指定
-    outDir: path.resolve(process.cwd(), 'build'),
+    outDir: path.resolve(process.cwd(), '..', '..', 'dist/app'),
   }
 });
