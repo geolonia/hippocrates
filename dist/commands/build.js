063cc427ee5ec2be90166ee8c476f1883bbc7568
naoki <n.globe.us@gmail.com>
2024-01-19 08:05:39 +0900

    app のコピーテスト

--- dist/commands/build.js
+++ dist/commands/build.js
@@ -5,19 +5,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.build = void 0;
 const path_1 = __importDefault(require("path"));
-const excel2json_1 = require("../lib/excel2json");
-const buildConfig_1 = require("../lib/buildConfig");
-const buildTypeScript_1 = require("../lib/buildTypeScript");
-const build = async (source) => {
-    const sourcePath = path_1.default.resolve(process.cwd(), source || '.');
-    const basePath = path_1.default.resolve((process.cwd(), 'node_modules', 'hippocrates'));
-    const appConfigPath = path_1.default.resolve(basePath, 'src', 'app', 'src');
-    const publicPath = path_1.default.resolve(basePath, 'src', 'app', 'public');
-    // sourcePath 内の Excel/CSV を json に変換する
-    await (0, excel2json_1.excelToJson)(sourcePath, publicPath);
-    // config.json と .env を生成する
-    await (0, buildConfig_1.buildConfig)(appConfigPath);
-    // TypeScript をビルドする
-    await (0, buildTypeScript_1.buildTypeScript)();
+// import { excelToJson } from '../lib/excel2json'
+// import { buildConfig} from '../lib/buildConfig'
+// import { buildTypeScript } from '../lib/buildTypeScript'
+const copyDirectory_1 = require("../lib/copyDirectory");
+const build = async (_source) => {
+    const basePath = path_1.default.resolve((process.cwd(), 'node_modules/hippocrates'));
+    // node_modules/hippocrates/dist/app を process.cwd() にコピーする
+    const appPath = path_1.default.resolve(basePath, 'dist', 'app');
+    const destPath = path_1.default.resolve(process.cwd(), 'build');
+    (0, copyDirectory_1.copyDirectory)(appPath, destPath);
+    // const sourcePath = path.resolve(process.cwd(), source || '.')
+    // const appConfigPath = path.resolve(basePath, 'src', 'app', 'src');
+    // const publicPath = path.resolve(basePath, 'src', 'app', 'public');
+    // // sourcePath 内の Excel/CSV を json に変換する
+    // await excelToJson(sourcePath, publicPath);
+    // // config.json と .env を生成する
+    // await buildConfig(appConfigPath);
+    // // TypeScript をビルドする
+    // await buildTypeScript();
 };
 exports.build = build;
--- src/commands/build.ts
+++ src/commands/build.ts
@@ -1,22 +1,30 @@
 import path from 'path'
-import { excelToJson } from '../lib/excel2json'
-import { buildConfig} from '../lib/buildConfig'
-import { buildTypeScript } from '../lib/buildTypeScript'
+// import { excelToJson } from '../lib/excel2json'
+// import { buildConfig} from '../lib/buildConfig'
+// import { buildTypeScript } from '../lib/buildTypeScript'
+import { copyDirectory } from '../lib/copyDirectory'
 
-export const build = async (source: string | undefined) => {
+export const build = async (_source: string | undefined) => {
 
-  const sourcePath = path.resolve(process.cwd(), source || '.')
+  const basePath = path.resolve((process.cwd(), 'node_modules/hippocrates'))
 
-  const basePath = path.resolve((process.cwd(), 'node_modules', 'hippocrates'))
-  const appConfigPath = path.resolve(basePath, 'src', 'app', 'src');
-  const publicPath = path.resolve(basePath, 'src', 'app', 'public');
+  // node_modules/hippocrates/dist/app を process.cwd() にコピーする
+  const appPath = path.resolve(basePath, 'dist', 'app');
+  const destPath = path.resolve(process.cwd(), 'build');
+  copyDirectory(appPath, destPath);
 
-  // sourcePath 内の Excel/CSV を json に変換する
-  await excelToJson(sourcePath, publicPath);
 
-  // config.json と .env を生成する
-  await buildConfig(appConfigPath);
+  // const sourcePath = path.resolve(process.cwd(), source || '.')
 
-  // TypeScript をビルドする
-  await buildTypeScript();
+  // const appConfigPath = path.resolve(basePath, 'src', 'app', 'src');
+  // const publicPath = path.resolve(basePath, 'src', 'app', 'public');
+
+  // // sourcePath 内の Excel/CSV を json に変換する
+  // await excelToJson(sourcePath, publicPath);
+
+  // // config.json と .env を生成する
+  // await buildConfig(appConfigPath);
+
+  // // TypeScript をビルドする
+  // await buildTypeScript();
 }
