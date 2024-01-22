f034b3e3998a941401a288dc8be6325d2e2fe00e
naoki <n.globe.us@gmail.com>
2024-01-23 01:29:42 +0900

    some fix

--- dist/lib/execPromise.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.execPromise = void 0;
-const child_process_1 = require("child_process");
-const util_1 = require("util");
-exports.execPromise = (0, util_1.promisify)(child_process_1.exec);
