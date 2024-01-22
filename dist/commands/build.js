1bcb2e549553e949956901fb30a3b38c4a35a18d
naoki <n.globe.us@gmail.com>
2024-01-23 01:21:02 +0900

    some fix

--- dist/commands/build.js
+++ dist/commands/build.js
@@ -1,27 +1,27 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.build = void 0;
-const path_1 = __importDefault(require("path"));
-const excel2json_1 = require("../lib/excel2json");
-// import { buildConfig} from '../lib/buildConfig'
-// import { buildTypeScript } from '../lib/buildTypeScript'
-const copyDirectory_1 = require("../lib/copyDirectory");
+// import path from 'path'
+// import { excelToJson } from '../lib/excel2json'
+// // import { buildConfig} from '../lib/buildConfig'
+// // import { buildTypeScript } from '../lib/buildTypeScript'
+// import { copyDirectory } from '../lib/copyDirectory'
+const defaultValues_1 = require("../lib/defaultValues");
 const build = async (source) => {
-    const workingDirPath = process.cwd();
-    const basePath = path_1.default.resolve((process.cwd(), 'node_modules/hippocrates'));
-    const buildPath = path_1.default.resolve(workingDirPath, 'build');
-    // node_modules/hippocrates/dist/app を process.cwd() にコピーする
-    const appPath = path_1.default.resolve(basePath, 'dist', 'app');
-    const destPath = path_1.default.resolve(process.cwd(), 'build');
-    (0, copyDirectory_1.copyDirectory)(appPath, destPath);
-    const sourcePath = path_1.default.resolve(process.cwd(), source || '.');
-    // const appConfigPath = path.resolve(basePath, 'src', 'app', 'src');
-    // const publicPath = path.resolve(basePath, 'src', 'app', 'public');
-    // sourcePath 内の Excel/CSV を json に変換する
-    await (0, excel2json_1.excelToJson)(sourcePath, buildPath);
+    console.log(source);
+    console.log(defaultValues_1.defaultValues.providerDir);
+    // const workingDirPath = process.cwd();
+    // const basePath = path.resolve((process.cwd(), 'node_modules/hippocrates'))
+    // const buildPath = path.resolve(workingDirPath, 'build');
+    // // node_modules/hippocrates/dist/app を process.cwd() にコピーする
+    // const appPath = path.resolve(basePath, 'dist', 'app');
+    // const destPath = path.resolve(process.cwd(), 'build');
+    // copyDirectory(appPath, destPath);
+    // const sourcePath = path.resolve(process.cwd(), source || '.')
+    // // const appConfigPath = path.resolve(basePath, 'src', 'app', 'src');
+    // // const publicPath = path.resolve(basePath, 'src', 'app', 'public');
+    // // sourcePath 内の Excel/CSV を json に変換する
+    // await excelToJson(sourcePath, buildPath);
     // // config.json と .env を生成する
     // await buildConfig(appConfigPath);
     // // TypeScript をビルドする
--- src/commands/build.ts
+++ src/commands/build.ts
@@ -1,27 +1,31 @@
-import path from 'path'
-import { excelToJson } from '../lib/excel2json'
-// import { buildConfig} from '../lib/buildConfig'
-// import { buildTypeScript } from '../lib/buildTypeScript'
-import { copyDirectory } from '../lib/copyDirectory'
+// import path from 'path'
+// import { excelToJson } from '../lib/excel2json'
+// // import { buildConfig} from '../lib/buildConfig'
+// // import { buildTypeScript } from '../lib/buildTypeScript'
+// import { copyDirectory } from '../lib/copyDirectory'
+import  { defaultValues } from '../lib/defaultValues'
 
 export const build = async (source: string | undefined) => {
 
-  const workingDirPath = process.cwd();
-  const basePath = path.resolve((process.cwd(), 'node_modules/hippocrates'))
-  const buildPath = path.resolve(workingDirPath, 'build');
+  console.log(source)
+  console.log(defaultValues.providerDir)
 
-  // node_modules/hippocrates/dist/app を process.cwd() にコピーする
-  const appPath = path.resolve(basePath, 'dist', 'app');
-  const destPath = path.resolve(process.cwd(), 'build');
-  copyDirectory(appPath, destPath);
+  // const workingDirPath = process.cwd();
+  // const basePath = path.resolve((process.cwd(), 'node_modules/hippocrates'))
+  // const buildPath = path.resolve(workingDirPath, 'build');
+
+  // // node_modules/hippocrates/dist/app を process.cwd() にコピーする
+  // const appPath = path.resolve(basePath, 'dist', 'app');
+  // const destPath = path.resolve(process.cwd(), 'build');
+  // copyDirectory(appPath, destPath);
 
 
-  const sourcePath = path.resolve(process.cwd(), source || '.')
-  // const appConfigPath = path.resolve(basePath, 'src', 'app', 'src');
-  // const publicPath = path.resolve(basePath, 'src', 'app', 'public');
+  // const sourcePath = path.resolve(process.cwd(), source || '.')
+  // // const appConfigPath = path.resolve(basePath, 'src', 'app', 'src');
+  // // const publicPath = path.resolve(basePath, 'src', 'app', 'public');
 
-  // sourcePath 内の Excel/CSV を json に変換する
-  await excelToJson(sourcePath, buildPath);
+  // // sourcePath 内の Excel/CSV を json に変換する
+  // await excelToJson(sourcePath, buildPath);
 
   // // config.json と .env を生成する
   // await buildConfig(appConfigPath);
