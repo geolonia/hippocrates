32b7f2a058d838c4ce2b67633d0280dab1bea514
naoki <n.globe.us@gmail.com>
2024-01-23 15:57:59 +0900

    update dependency

--- dist/lib/buildTypeScript.js
+++ dist/lib/buildTypeScript.js
@@ -5,7 +5,7 @@ const execPromise_1 = require("./execPromise");
 const defaultValues_1 = require("./defaultValues");
 const buildTypeScript = async (env) => {
     try {
-        await (0, execPromise_1.execPromise)(`cd ${defaultValues_1.defaultValues.providerDir} && npm install && react-scripts build`, { env });
+        await (0, execPromise_1.execPromise)(`cd ${defaultValues_1.defaultValues.providerDir} && react-scripts build`, { env });
     }
     catch (error) {
         console.error(`実行エラー: ${error}`);
