10442970da580e7037701bdf29e35009c52bbe48
naoki <n.globe.us@gmail.com>
2024-01-23 18:02:02 +0900

    some fix

--- dist/commands/build.js
+++ dist/commands/build.js
@@ -5,37 +5,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.build = void 0;
 const path_1 = __importDefault(require("path"));
-// import { excelToJson } from '../lib/excel2json'
+const excel2geojson_1 = require("../lib/excel2geojson");
 const buildConfig_1 = require("../lib/buildConfig");
-// import { buildTypeScript } from '../lib/buildTypeScript'
 const copyDirectory_1 = require("../lib/copyDirectory");
 const buildTypeScript_1 = require("../lib/buildTypeScript");
 const defaultValues_1 = require("../lib/defaultValues");
 const fs_1 = __importDefault(require("fs"));
-const build = async (_source) => {
+const build = async (source) => {
     await (0, buildConfig_1.buildConfig)();
     const configPath = path_1.default.resolve(defaultValues_1.defaultValues.providerDir, 'config.json');
     const config = JSON.parse(fs_1.default.readFileSync(configPath, 'utf8'));
     // 現在の環境変数にカスタム環境変数をマージ
     const env = Object.assign({}, process.env, config);
+    // sourcePath 内の Excel/CSV を json に変換する
+    const sourcePath = source ? path_1.default.resolve(process.cwd(), source) : process.cwd();
+    await (0, excel2geojson_1.excelToGeojson)(sourcePath);
     await (0, buildTypeScript_1.buildTypeScript)(env);
     const innerNPMPath = path_1.default.resolve(defaultValues_1.defaultValues.providerDir, 'build');
     (0, copyDirectory_1.copyDirectory)(innerNPMPath, defaultValues_1.defaultValues.buildDir);
-    // const workingDirPath = process.cwd();
-    // const basePath = path.resolve((process.cwd(), 'node_modules/hippocrates'))
-    // const buildPath = path.resolve(workingDirPath, 'build');
-    // // node_modules/hippocrates/dist/app を process.cwd() にコピーする
-    // const appPath = path.resolve(basePath, 'dist', 'app');
-    // const destPath = path.resolve(process.cwd(), 'build');
-    // copyDirectory(appPath, destPath);
-    // const sourcePath = path.resolve(process.cwd(), source || '.')
-    // // const appConfigPath = path.resolve(basePath, 'src', 'app', 'src');
-    // // const publicPath = path.resolve(basePath, 'src', 'app', 'public');
-    // // sourcePath 内の Excel/CSV を json に変換する
-    // await excelToJson(sourcePath, buildPath);
-    // // config.json と .env を生成する
-    // await buildConfig(appConfigPath);
-    // // TypeScript をビルドする
-    // await buildTypeScript();
 };
 exports.build = build;
---
+++ dist/lib/excel2geojson.js
@@ -0,0 +1,84 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.excelToGeojson = void 0;
+const excel2csv_1 = require("./excel2csv");
+const promises_1 = require("fs/promises");
+const klaw_1 = __importDefault(require("klaw"));
+//@ts-ignore
+const csv2geojson_1 = __importDefault(require("csv2geojson"));
+const error_1 = __importDefault(require("./error"));
+const path_1 = __importDefault(require("path"));
+const defaultValues_1 = require("./defaultValues");
+const _excelToGeojson = async (inputDir) => {
+    const promises = [];
+    // 指定したディレクトリ直下のファイルのみを対象にする
+    for await (const file of (0, klaw_1.default)(inputDir, { depthLimit: 0 })) {
+        let csvData;
+        if (file.path.endsWith(".xlsx")) {
+            const excelPath = file.path;
+            try {
+                csvData = await (0, excel2csv_1.excel2csv)(excelPath);
+            }
+            catch (err) {
+                if (err instanceof Error) {
+                    if (err.message === "FILE_ENDED") {
+                        throw new error_1.default("fileEnded", excelPath);
+                    }
+                }
+                throw new error_1.default("excelToGeojson", excelPath);
+            }
+        }
+        else if (file.path.endsWith(".csv")) {
+            csvData = await (0, promises_1.readFile)(file.path, 'utf-8');
+        }
+        else if (file.path.endsWith(".geojson")) {
+            // TODO: GeoJSON を data.geojson 形式に変換する
+            // geojson の場合は 拡張子を json にしてdefaultValues.providerDir にコピーする
+            promises.push((0, promises_1.copyFile)(file.path, path_1.default.resolve(defaultValues_1.defaultValues.providerDir, 'data.geojson')));
+            // 1ファイルのみを対象にするために break する
+            break;
+        }
+        if (csvData) {
+            try {
+                csv2geojson_1.default.csv2geojson(csvData, async (_err, geojson) => {
+                    await (0, promises_1.writeFile)(path_1.default.resolve(defaultValues_1.defaultValues.providerDir, 'data.geojson'), JSON.stringify(geojson));
+                });
+            }
+            catch (err) {
+                throw new error_1.default("csvToGeoJson", file.path);
+            }
+            // 1ファイルのみを対象にするために break する
+            break;
+        }
+    }
+    await Promise.all(promises);
+};
+const excelToGeojson = async (inputDir) => {
+    try {
+        await _excelToGeojson(inputDir);
+    }
+    catch (err) {
+        if (err instanceof error_1.default) {
+            switch (err.conversionType) {
+                case "excelToGeojson":
+                    throw new Error(`Error: Excel ファイルを ${err.filePath} GeoJSON に変換できませんでした。`);
+                    break;
+                case "fileEnded":
+                    throw new Error(`Error: データが空になっているか、Excel ファイルが破損している可能性があります。`);
+                    break;
+                case "csvToGeoJson":
+                    throw new Error(`Error: CSV データ ${err.filePath} を GeoJSON に変換できませんでした。`);
+                    break;
+                default:
+                    throw new Error(err.message);
+            }
+        }
+        else {
+            throw new Error(err.message);
+        }
+    }
+};
+exports.excelToGeojson = excelToGeojson;
