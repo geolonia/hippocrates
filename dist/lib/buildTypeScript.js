7ecc26dfb83589f4ad14118c2b24681a414e2368
naoki <n.globe.us@gmail.com>
2024-01-23 14:18:24 +0900

    some fix

--- dist/lib/buildTypeScript.js
+++ dist/lib/buildTypeScript.js
@@ -1,20 +1,11 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.buildTypeScript = void 0;
-const path_1 = __importDefault(require("path"));
 const execPromise_1 = require("./execPromise");
 const defaultValues_1 = require("./defaultValues");
-const fs_1 = __importDefault(require("fs"));
 const buildTypeScript = async (env) => {
     try {
-        await (0, execPromise_1.execPromise)(`cd ${defaultValues_1.defaultValues.providerDir}`);
-        if (!fs_1.default.existsSync(path_1.default.join(defaultValues_1.defaultValues.providerDir, 'node_modules'))) {
-            await (0, execPromise_1.execPromise)(`npm install`);
-        }
-        await (0, execPromise_1.execPromise)(`react-scripts build`, { env });
+        await (0, execPromise_1.execPromise)(`cd ${defaultValues_1.defaultValues.providerDir} && react-scripts build`, { env });
     }
     catch (error) {
         console.error(`実行エラー: ${error}`);
--- src/lib/buildTypeScript.ts
+++ src/lib/buildTypeScript.ts
@@ -1,19 +1,10 @@
-import path from "path";
 import { execPromise } from "./execPromise";
 import { defaultValues } from "./defaultValues";
-import fs from 'fs'
 
 export const buildTypeScript = async (env: NodeJS.ProcessEnv) => {
   try {
 
-    await execPromise(`cd ${defaultValues.providerDir}`);
-
-    if (!fs.existsSync(path.join(defaultValues.providerDir, 'node_modules'))) {
-
-      await execPromise(`npm install`);
-    }
-
-    await execPromise(`react-scripts build`, {env});
+    await execPromise(`cd ${defaultValues.providerDir} && react-scripts build`, {env});
 
   } catch (error) {
     console.error(`実行エラー: ${error}`);
