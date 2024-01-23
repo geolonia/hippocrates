540f7318d3c9ed3c4ca104e4500f305a0aa1758a
naoki <n.globe.us@gmail.com>
2024-01-24 07:18:11 +0900

    fix pc view

--- provider/src/index.tsx
+++ provider/src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from "react-router-dom";
-import Container from './Container';
+import App from './App'
 import './index.scss'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
-      <Container />
+      <App />
     </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
