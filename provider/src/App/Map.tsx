bb90503de4bd6cb25039d6b72c218abbd094fbc9
naoki <n.globe.us@gmail.com>
2024-01-24 09:08:52 +0900

    some fix

--- provider/src/App/Map.tsx
+++ provider/src/App/Map.tsx
@@ -32,20 +32,6 @@ const hidePoiLayers = (map: any) => {
   }
 }
 
-const parseHash = (url?: Location | URL) => {
-  const qstr = (url || window.location).hash.substring(2);
-  const q = new URLSearchParams(qstr);
-  return q;
-};
-
-const updateHash = (q: URLSearchParams) => {
-
-  const hash = q.toString();
-  if (hash) {
-    window.location.hash = `#/?${q.toString().replace(/%2F/g, '/')}`;
-  }
-};
-
 const addLatLngToProperties = (feature: GeoJSONFeature) => {
   // @ts-ignore
   const coordinates = feature.geometry.coordinates
@@ -58,7 +44,6 @@ const Content = (props: Props) => {
   const mapNode = React.useRef<HTMLDivElement>(null);
   const [mapObject, setMapObject] = React.useState<any>()
   const [shop, setShop] = React.useState<Pwamap.ShopData | undefined>(undefined)
-  const [ zLatLngString, setZLatLngString ] = React.useState<string>('');
 
   const addMarkers = (mapObject: any) => {
 
@@ -161,8 +146,6 @@ const Content = (props: Props) => {
       })
 
       setCluster(mapObject)
-
-
     });
 
   }
@@ -172,15 +155,6 @@ const Content = (props: Props) => {
     addMarkers(mapObject)
 
   }, [mapObject])
-
-  React.useEffect(() => {
-    const hash = parseHash();
-    if (zLatLngString) {
-      hash.set('map', zLatLngString);
-    }
-    updateHash(hash);
-
-  }, [ zLatLngString ]);
 
   React.useEffect(() => {
     // Only once reder the map.
@@ -198,42 +172,9 @@ const Content = (props: Props) => {
       fitBoundsOptions: { padding: 50 },
     });
 
-    const hash = parseHash();
-    if (hash && hash.get('map')) {
-
-      const latLngString = hash.get('map') || '';
-      const zlatlng = latLngString.split('/');
-
-      const zoom = zlatlng[0]
-      const lat = zlatlng[1]
-      const lng = zlatlng[2]
-
-      map.flyTo({center: [lng, lat], zoom});
-
-    } else if (props.bounds) {
-
-      map.fitBounds(props.bounds, { padding: 50 })
-
-    }
-
     const onMapLoad = () => {
       hidePoiLayers(map)
       setMapObject(map)
-
-      map.on('moveend', () => {
-        // see: https://github.com/maplibre/maplibre-gl-js/blob/ba7bfbc846910c5ae848aaeebe4bde6833fc9cdc/src/ui/hash.js#L59
-        const center = map.getCenter(),
-          rawZoom = map.getZoom(),
-          zoom = Math.round(rawZoom * 100) / 100,
-          // derived from equation: 512px * 2^z / 360 / 10^d < 0.5px
-          precision = Math.ceil((zoom * Math.LN2 + Math.log(512 / 360 / 0.5)) / Math.LN10),
-          m = Math.pow(10, precision),
-          lng = Math.round(center.lng * m) / m,
-          lat = Math.round(center.lat * m) / m,
-          zStr = Math.ceil(zoom);
-
-        setZLatLngString(`${zStr}/${lat}/${lng}`);
-      });
     }
 
     const orienteationchangeHandler = () => {
